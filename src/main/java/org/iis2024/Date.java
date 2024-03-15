4a55edbe452a4d3a7b02753ad1c8a64295b9cd70
AlRiGu <lasierra2005@gmail.com>
2024-03-15 12:10:32 +0100

    Metodo añadido por el propietario

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -60,6 +60,9 @@ public class Date {
     this.month = month;
     this.year = year;
   }
+  public void methodAddedByOwner(){
+    System.out.println("Method added by the owner");
+  }
 
   /**
    * Validates the date according to the predefined rules: the year must be within 1900 to 2050, the
